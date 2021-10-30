6d30eb2e594f848de17ec823b749b6ac36595c9c
linhlinh18121 <93236603+linhlinh18121@users.noreply.github.com>
2021-10-30 11:32:45 +0700

    Add files via upload

---
+++ cop cái này vào đầu mỗi trang admin nhé.php
@@ -0,0 +1,17 @@
+<?php 
+    // Mục đích kiểm tra xem bạn có quyền truy cập trang này không thông qua BIẾN $_SESSION['da_dang_nhap']
+    session_start();
+    if (!isset($_SESSION["da_dang_nhap"]) ){
+        echo "
+            <script type='text/javascript'>
+                window.alert('Bạn không có quyền truy cập');
+            </script>
+        ";
+
+        echo "
+            <script type='text/javascript'>
+                window.location.href='dang_nhap.php';
+            </script>
+        ";
+    }
+;?>
